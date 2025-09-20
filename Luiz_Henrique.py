fb2cdd124221d496aea2f3c3d90de614ccb22d39
LuHenRe <154239376+LuHenRe@users.noreply.github.com>
2025-09-19 22:30:27 -0300

    Add files via upload

--- Luiz_Henrique.py
+++ Luiz_Henrique.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 '''
 Faz um gráfico para uma função específica com a variável 'f' e 'a, b' como pontos do eixo das abscissas do gráfico 
 '''
-
 def grafico(f, a, b):
     x = np.linspace(a, b, 100)
     plt.plot(x, f(x))
@@ -14,11 +13,10 @@ def grafico(f, a, b):
     plt.grid(True)
     plt.show()
 
+
 """
 Faz a bisseção de um intervalo [a, b] para encontrar uma raiz real
 """
-
-
 def bissecao(funcao, a, b, tol=1e-4):
     raiz = (a + b) / 2
 
@@ -44,3 +42,20 @@ def secante(f, a, b, tol=1e-6, max_iter=100):
         a = b
         b = c
     return None # Não converge
+
+
+def falsa_posicao(f, a, b, tol=1e-6, max_iter=100):
+#Encontra a raiz, no intervalo [x0, x1], da equação definida em fParada: no máximo, max_iter, iterações ou diferença entre
+#os limites do intervalo menor que tol.
+    if f(a) * f(b) >= 0:
+        print("ops! f(a) e f(b) devem ter sinais opostos")
+        return None
+    for i in range(max_iter):
+        c = (a * f(b) - b * f(a)) / (f(b) - f(a))
+        if abs(f(c)) < tol:
+            return c
+        if f(c) * f(a) < 0:
+            b = c
+        else:
+            a = c
+    return None # não converge
